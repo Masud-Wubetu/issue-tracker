170f835996be2c1b30921c3f68146df05099131e
Masud <wubetumasud@gmail.com>
2026-03-25 08:09:23 +0300

    Refactor for code reusability of NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,12 +3,20 @@ import React from 'react'
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+  const link = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' }
+  ]
+
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href='/'><AiFillBug/></Link>
         <ul className='flex space-x-6'>
-            <li><Link className="text-zinc-500" href='/'>Dashboard</Link></li>
-            <li><Link href='/issues'>Issues</Link></li>
+            {link.map(link => 
+            <Link 
+                key={link.href} 
+                className="text-zinc-500 hover:text-zinc-800 transition-colors" 
+                href={link.href}>{link.label}</Link>)}
         </ul>
         
     </nav>
