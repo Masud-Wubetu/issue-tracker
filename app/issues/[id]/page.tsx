64eecc1a61990c1965a99b82c1281a78d6fce2dd
Masud <wubetumasud@gmail.com>
2026-04-06 11:46:21 +0300

    show issue details

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { prisma } from '@/prisma/client'
+import { notFound } from 'next/navigation';
+import React from 'react'
+
+interface Props {
+    params: { id: string }
+}
+
+const IssueDetailpage = async ({ params }: Props ) => {
+    if (typeof params.id !== 'number') notFound();
+    
+    const { id } = await params;
+
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(id) }
+    });
+
+    if (!issue) notFound();
+
+  return (
+    <div>
+        <p>{issue.title}</p>
+        <p>{issue.description}</p>
+        <p>{issue.status}</p>
+        <p>{issue.createdAt.toDateString()}</p>
+    </div>
+  )
+}
+
+export default IssueDetailpage
