55feb99b10c9f588482651ea9f7d8c75109bd1ad
Your Name <wubetumasud@gmail.com>
2026-04-22 00:13:10 +0300

    typscript debug

--- app/component/Link.tsx
+++ app/component/Link.tsx
@@ -2,14 +2,17 @@
 import NextLink from 'next/link'
 import { Link as RadixLink } from '@radix-ui/themes'
 
+import { ReactNode } from 'react';
+
 interface Props {
-    href: string,
-    children: string
+    href: string;
+    children: ReactNode;
+    className?: string;
 }
 
-const Link = ({ href, children }: Props) => {
+const Link = ({ href, children, className }: Props) => {
   return (
-    <RadixLink asChild>
+    <RadixLink asChild className={className}>
       <NextLink href={href}>{children}</NextLink>
     </RadixLink>
   )
